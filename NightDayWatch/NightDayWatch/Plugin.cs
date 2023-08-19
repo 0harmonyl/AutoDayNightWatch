e65547847939a522b502722c560097aa917a12c9
0harmonyl <142665949+0harmonyl@users.noreply.github.com>
2023-08-19 13:33:09 +0200

    initial commit

---
+++ NightDayWatch/NightDayWatch/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace NightDayWatch
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.9")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Events.GameInitialized += GameInitialized;
+        }
+
+        void GameInitialized(object sender, EventArgs e)
+        {
+            OnEnable();
+        }
+
+        void OnEnable()
+        {
+            /* Mod set up in here as code in the OnEnable void runs at the start and when the mod is enabled */
+
+            HarmonyPatches.ApplyHarmonyPatches();
+
+            Setup();
+        }
+
+        void OnDisable()
+        {
+            /* Undoing mod set up in here as this is called when CI disables a mod */
+
+            HarmonyPatches.RemoveHarmonyPatches();
+
+            UndoSetup();
+        }
+
+        void Setup()
+        {
+            GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L/LMAFN.LEFT.").SetActive(true);
+        }
+
+        void UndoSetup()
+        {
+            GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L/LMAFN.LEFT.").SetActive(false);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AutoDayNightWatch
- A simple mod to give you the day night watch, which is also toggleable in Computer Interface
+ A simple mod to give you the day night watch, which is also toggleable in Computer Interface.
+ Made this in like 10 minutes. Might make it a custom model down the line but for now it just enables the cosmetic.
